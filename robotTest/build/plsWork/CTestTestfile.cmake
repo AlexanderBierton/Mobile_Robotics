3ea9442f24a1741a4e355404d772b4a8278db722
AlexanderBierton <alexanderbierton@googlemail.com>
2017-02-08 14:01:21 +0000

    First Commit

---
+++ robotTest/build/plsWork/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/computing/robotTest/src/plsWork
+# Build directory: /home/computing/robotTest/build/plsWork
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
