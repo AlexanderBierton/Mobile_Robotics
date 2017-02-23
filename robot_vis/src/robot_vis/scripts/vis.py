b4ff4c399c189a138aa2313124badfac173a0e08
AlexanderBierton <alexanderbierton@googlemail.com>
2017-02-23 10:57:02 +0000

    Working navigation to green object

--- robot_vis/src/robot_vis/scripts/vis.py
+++ robot_vis/src/robot_vis/scripts/vis.py
@@ -15,18 +15,18 @@ class image_converter:
         cv2.namedWindow("Image window", 1)
         cv2.startWindowThread()
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/turtlebot_2/camera/rgb/image_raw", Image, self.callback)
-        self.wheel_sub = rospy.Subscriber("/wheel_vel_left", Float32, self.wheelcallback)
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+       # self.wheel_sub = rospy.Subscriber("/wheel_vel_left", Float32, self.wheelcallback)
 
     def callback(self, data):
-        pub = rospy.Publisher('/turtlebot_2/cmd_vel', Twist)
+        pub = rospy.Publisher('/cmd_vel', Twist)
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError, e:
             print e
         t = Twist()
-        t.angular.z = 0.2
-        pub.publish(t)
+        #t.angular.z = 1.0
+        #pub.publish(t)
         bgr_thresh = cv2.inRange(cv_image,
                                  numpy.array((200, 230, 230)),
                                  numpy.array((255, 255, 255)))
@@ -34,14 +34,14 @@ class image_converter:
         hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         hsv_thresh = cv2.inRange(hsv_img,
                                  numpy.array((90, 150, 0)),
-                                 numpy.array((180, 255, 255)))
+                                 numpy.array((180, 250, 250)))
 
         print numpy.mean(hsv_img[:, :, 0])
         print numpy.mean(hsv_img[:, :, 1])
         print numpy.mean(hsv_img[:, :, 2])
         
-        lower_yellow = numpy.array([60, 10, 90])
-        upper_yellow = numpy.array([100, 255, 255])
+        lower_yellow = numpy.array([60, 10, 100])
+        upper_yellow = numpy.array([100, 225, 200])
         
         bgr_contours, hierachy = cv2.findContours(bgr_thresh.copy(),
                                                   cv2.RETR_TREE,
@@ -58,27 +58,37 @@ class image_converter:
         mask = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
         
         h, w, d = cv_image.shape        
-        #search_top = 3*h/4
-        #search_bot = 3*h/4 + 20
-        #mask[0:search_top, 0:w] = 0
-        #mask[search_bot:h, 0:w] = 0
-        #M = cv2.moments(mask)
-        #if M['n00'] > 0:
+        search_top = 3*h/4
+        search_bot = 3*h/4 + 20
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        M = cv2.moments(mask)
+        if M['m00'] > 0:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            cv2.circle(cv_image, (cx, cy), 20, (0, 0, 255), -1)
+            err = cx - w/2
+            t.linear.x = 0.2
+            t.angular.z = -float(err) /100
+            pub.publish(t)
+        #cv2.imshow("window", cv_image)
+        #cv2.waitkey(3)
             
         masked = cv2.bitwise_and(cv_image, cv_image, mask = mask)
-        #m = numpy.mean(masked)
+        m = numpy.mean(masked)
         #print "this is mean ", m
-        cv2.imshow("Image window", masked)
-        #meanOut = rospy.Publisher("/colour_output", String)
-        #s = String()
-        #s.data = str(m)
-        #meanOut.publish(s)
-    def wheelcallback(self, whldata):
-        pub = rospy.Publisher('/turtlebot_2/cmd_vel', Twist)
-        print "Yo mama ",whldata
-        u = Twist()
-        pub.publish(u)
-        u.angular.z = whldata
+        cv2.imshow("Image window", cv_image)
+        meanOut = rospy.Publisher("/colour_output", String)
+        s = String()
+        s.data = str(m)
+        meanOut.publish(s)
+    #def wheelcallback(self, whldata):
+     #   pub = rospy.Publisher('/turtlebot_2/cmd_vel', Twist)
+      #  print "Yo mama ",whldata
+       # u = Twist()
+        #pub.publish(u)
+        #u.angular.z = whldata
+        #rospy.signal_shutdown('Information Received')
         
 
 image_converter()
